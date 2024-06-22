116376607d06818a70b74c92c729fe89db2425b9
Huuquan1602 <quannguyenhuu23@gmail.com>
2024-06-22 15:33:22 +0700

    Leedcode20-2

---
+++ 2.ValidParentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
+
+
+
+# sol = Solution()
+# s = '()]'
+# a = sol.isValid(s)
+# print (a)
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Leedcode
 
 
 ###1. [Two Sum](https://leetcode.com/problems/two-sum/description/)
+
+
+###2. [Valid Parentheses](https://leetcode.com/problems/valid-parentheses/description/)
